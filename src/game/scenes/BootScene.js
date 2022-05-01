e8b4482fb39b55262bd745200d4dc2f5a743fe2e
Makochaaan <104635152+Makochaaan@users.noreply.github.com>
2022-05-01 11:13:12 +0900

    Update BootScene.js

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -9,6 +9,9 @@ import clapMp3 from '@/game/assets/clap.mp3'
 import clapOgg from '@/game/assets/clap.ogg'
 import thudMp3 from '@/game/assets/thud.mp3'
 import thudOgg from '@/game/assets/thud.ogg'
+import key_b from '@/game/assets/key_b.png'
+import fanfareMp3 from '@/game/assets/fanfare.mp3'
+import correctWav from '@/game/assets/correct.wav'
 
 export let mondai = [];
 export let keyList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'hyphen', 'hat', 'doller', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'at', 'Lkakko', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'semicoron', 'coron', 'Rkakko', 'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'comma', 'period', 'slash', 'under', 'enter']
@@ -21,10 +24,10 @@ export default class BootScene extends Scene {
     // ロードするやつ
     this.load.image('sky', sky)
     this.load.image('bomb', bomb)
-    this.load.audio('thud', [thudMp3, thudOgg])
-    this.load.audio('clap', [clapMp3, clapOgg])
     this.load.image('happy_bg', happy_bg)
     this.load.image('titleBack',titleBack)
+    this.load.image('key_b',key_b)
+
     this.whiteKeyImages={};
     this.orangeKeyImages={}
     for(var i=0; i < keyList.length; i++){
@@ -37,6 +40,10 @@ export default class BootScene extends Scene {
     // this.load.image('keyb', [keyb]);
     // 音声
     this.load.audio('miss', missWav)
+    this.load.audio('fanfare',fanfareMp3)
+    this.load.audio('correct',correctWav)
+    this.load.audio('thud', [thudMp3, thudOgg])
+    this.load.audio('clap', [clapMp3, clapOgg])
 
     getFile();
   }
