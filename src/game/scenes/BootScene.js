3510dcc9302ebf5bf918454770c27e3dc15490c3
Makochaaan <arowana326.315@gmail.com>
2022-05-01 10:40:29 +0900

    TitleScene added completely

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -2,6 +2,7 @@ import { Scene } from 'phaser'
 import sky from '@/game/assets/sky.png'
 import bomb from '@/game/assets/bomb.png'
 import titleBack from '@/game/assets/titleBack.png'
+import titleLogo from '@/game/assets/titleLogo.png'
 import thudMp3 from '@/game/assets/thud.mp3'
 import thudOgg from '@/game/assets/thud.ogg'
 import clapMp3 from '@/game/assets/clap.mp3'
@@ -17,6 +18,7 @@ export default class BootScene extends Scene {
     this.load.image('sky', sky)
     this.load.image('bomb', bomb)
     this.load.image('titleBack', titleBack)
+    this.load.image('titleLogo', titleLogo)
     this.load.audio('thud', [thudMp3, thudOgg])
     this.load.audio('clap', [clapMp3, clapOgg])
   }
--- src/game/scenes/TitleScene.js
+++ src/game/scenes/TitleScene.js
@@ -17,17 +17,22 @@ export default class TitleScene extends Phaser.Scene {
 
     // ===== ゲームオブジェクト =====
     // Sound
-    if (this.sfx_gong1 == null) this.sfx_gong1 = this.sound.add('clap', {volume: 0.1});
+    if (this.sfx_gong1 == null) this.sfx_gong1 = this.sound.add('clap', {volume: 0.3});
 
     // Image
     this.titleBackGroundImage = this.add.sprite(400, 320, 'titleBack');
     this.titleBackGroundImage.displayWidth = 1200;
     this.titleBackGroundImage.scaleY = this.titleBackGroundImage.scaleX;
 
+    this.titleLogoImage = this.add.sprite(380, 300, 'titleLogo');
+    this.titleLogoImage.displayWidth = 550;
+    this.titleLogoImage.scaleY = this.titleLogoImage.scaleX;
+    
+
     // Text
-    this.notionText = this.add.text(380, 200, '！注意！ここから先は、音声が流れます').setOrigin(0.5).setFontSize(32).setColor('Yellow');
-    this.pushSpaceText = this.add.text(380, 380, 'スペースキーでゲーム開始').setOrigin(0.5).setFontSize(32).setFontFamily('monospace, serif').setColor('Yellow');
-    // ===== 開始 =====
+    this.notionText = this.add.text(380, 530, '！注意！ここから先は、音声が流れます').setOrigin(0.5).setFontSize(32).setColor('Yellow');
+    this.pushSpaceText = this.add.text(380, 420, 'スペースキーでゲーム開始').setOrigin(0.5).setFontSize(32).setFontFamily('monospace, serif').setColor('Yellow');
+    
     // ===== イベント処理 =====
     this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
@@ -36,11 +41,12 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   update () {
+    // ふわふわ文字
      this.fontSize += this.addFontSize;
   if(this.fontSize < 32 || 36 < this.fontSize ) this.addFontSize *= -1;
     this.pushSpaceText.setFontSize(this.fontSize);
 
-
+　　// ===== 開始 =====
   if (Phaser.Input.Keyboard.JustDown(this.spacebar))
       {
 　 　this.sfx_gong1.play();
